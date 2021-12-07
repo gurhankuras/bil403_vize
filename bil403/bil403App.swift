dba4965761b530360116e85fbe4419c2c1b526bf
gurhankuras <gurhankuras@hotmail.com>
2021-12-07 14:10:43 +0300

    Initial Commit

---
+++ bil403/bil403App.swift
@@ -0,0 +1,17 @@
+//
+//  bil403App.swift
+//  bil403
+//
+//  Created by Gürhan Kuraş on 12/7/21.
+//
+
+import SwiftUI
+
+@main
+struct bil403App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
